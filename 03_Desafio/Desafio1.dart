a5918a85e4fafae9113fc4a91aa8e3c3c99ad44f
manuellarinaldi <manuellarinaldi@gmail.com>
2026-08-17 20:35:26 -0300

    update

---
+++ 03_Desafio/Desafio1.dart
@@ -0,0 +1,23 @@
+
+
+
+
+
+void main(){
+  //Variavel do tipo STRING para armazenar o nome do aluno
+  String nome = "Manuella";
+  //Variavel do tipo INT para armazenar a idade do aluno
+  int idade = 19;
+  //Variavel do tipo STRING para armazenar o curso do aluno
+  String curso = "Analise e desenvolvimento de sistemas";
+  //Variavel do tipo DOUBLE para armazenar a nota do aluno
+  double nota = 9;
+  //Variavel do tipo BOOL para armazenar o status de aprovacao do aluno
+   bool aprovado = true;
+  //Exibindo dados do aluno no console
+   print('Nome: $nome');
+   print('Idade: $idade');
+   print('Curso: $curso');
+   print('Nota: $nota');
+   print('Aprovado: $aprovado');
+}
---
+++ 03_Desafio/Desafio2.dart
@@ -0,0 +1,12 @@
+ void main(){
+  String produto = "Notebook";
+  int quantidade =  2;
+  double preco = 3500.00;
+  bool disponivel = true;
+
+  print('Produto: $produto');
+  print('Quantidade: $quantidade');
+  print('Preço: $preco');
+  print('Disponível: $disponivel');
+
+ }
