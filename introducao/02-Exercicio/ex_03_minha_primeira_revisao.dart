c39e796e73d61f165181b91da69c71acc4101885
manuellarinaldi <manuellarinaldi@gmail.com>
2026-08-17 20:55:04 -0300

    update

---
+++ introducao/02-Exercicio/ex_03_minha_primeira_revisao.dart
@@ -0,0 +1,11 @@
+void main(){
+
+print('O que é dart imprime?');
+print('');
+
+print( 'Dart é a linguagem usada para criar aplicativos com flutter');
+
+print('');
+
+print('Dart é a linguagem usada para criar aplicativos com flutter');
+}
---
+++ introducao/02-Exercicio/exercicio01_primeiro_print.dart
@@ -0,0 +1,15 @@
+//Esse programa demonstra como exibir informações pessoais no console
+
+void main() {
+
+  //Imprimindo informações pessoais no console
+  print("Nome: Manuella Rinaldi");
+  
+  //Imprimindo informações de cidade de origem
+  print("Cidade de origem: São Paulo - SP");
+
+  //Imprimindo informações do curso que você está aprendendo
+  print("Objetivo: apreender Dart e Flutter");
+
+  //Imprimindo 
+}
